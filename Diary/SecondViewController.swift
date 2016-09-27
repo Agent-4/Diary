f50bfb9971e6b7c686181f0e3cded070fed80090
Agent 4 <1028594899@qq.com>
2016-09-27 17:41:51 +0800

    Initial Commit

---
+++ Diary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Diary
+//
+//  Created by moxiaohao on 16/9/27.
+//  Copyright © 2016年 moxiaohao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
