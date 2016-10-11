67783d45a7071b00a01ba6c33cd63fce22c55f6a
莫晓豪 <1028594899@qq.com>
2016-10-11 21:09:09 +0800

    Tabbar 更新

---
+++ Diary/DiaryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DiaryViewController.swift
+//  Diary
+//
+//  Created by moxiaohao on 16/9/30.
+//  Copyright © 2016年 moxiaohao. All rights reserved.
+//
+
+import UIKit
+
+class DiaryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Diary/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  Diary
+//
+//  Created by moxiaohao on 16/9/27.
+//  Copyright © 2016年 moxiaohao. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Tabbar 颜色设置
+        self.tabBarController?.tabBar.tintColor = UIColor (colorLiteralRed: 10/255, green: 195/255, blue: 10/255, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
