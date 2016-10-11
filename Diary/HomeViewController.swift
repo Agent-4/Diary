b2d04ad8f8af00fa8c850b6e7f63390f5cd664a2
莫晓豪 <1028594899@qq.com>
2016-10-11 22:05:24 +0800

    Tabbar 颜色设置

--- Diary/HomeViewController.swift
+++ Diary/HomeViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //Tabbar 颜色设置
-        self.tabBarController?.tabBar.tintColor = UIColor (colorLiteralRed: 10/255, green: 195/255, blue: 10/255, alpha: 1)
+        self.tabBarController?.tabBar.tintColor = UIColor (colorLiteralRed: 50/255, green: 205/255, blue: 50/255, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
